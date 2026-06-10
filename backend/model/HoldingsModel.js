e467cb60c11f9eccbf610102557283ef2a0eb84e
Nitish2004k <nitishbaluakumar@gmail.com>
2026-06-10 23:48:30 +0530

    fix: correct schema import path casing

--- backend/model/HoldingsModel.js
+++ backend/model/HoldingsModel.js
@@ -1,6 +1,6 @@
 const { model } = require("mongoose");
 
-const { HoldingsSchema } = require("../schemas/HoldingsSchema");
+const HoldingsSchema = require('../Schemas/HoldingsSchema');
 
 const HoldingsModel = new model("holding", HoldingsSchema);
 
