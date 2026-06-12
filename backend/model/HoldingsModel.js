01fd4ab6bbd35ed42ff9703142d3570f49e69ede
Nitish2004k <nitishbaluakumar@gmail.com>
2026-06-12 19:54:33 +0530

    fix: lowercase schemas path in HoldingsModel

--- backend/model/HoldingsModel.js
+++ backend/model/HoldingsModel.js
@@ -1,6 +1,7 @@
 const { model } = require("mongoose");
 
-const HoldingsSchema = require('../Schemas/HoldingsSchema');
+// Ye karo (lowercase s):
+const HoldingsSchema = require('../schemas/HoldingsSchema');
 
 const HoldingsModel = new model("holding", HoldingsSchema);
 
